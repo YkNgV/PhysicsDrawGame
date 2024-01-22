f267a5a66e858607358a98900fbad3f73c5701d7
YkNgV <ykngmp@gmail.com>
2024-01-23 00:10:46 +0800

    final build

--- TerryTemplate/Assets/Scripts/SlowMo.cs
+++ TerryTemplate/Assets/Scripts/SlowMo.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.iOS;
+//using UnityEngine.iOS;
 using UnityEngine.UI;
 
 public class SlowMo : MonoBehaviour
